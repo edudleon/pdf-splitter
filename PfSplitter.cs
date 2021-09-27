007431f241486859b1068ce878d959d2ae9413b0
Eduardo de Leon <ed.leonr@gmail.com>
2021-09-27 10:45:56 -0500

    Add project files.

---
+++ PfSplitter.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.IO;
+
+namespace PdfSplitter
+{
+    class PfSplitter
+    {
+        static PfSplitter()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        public static void Split(string inputFilePath, string outputFilePath, string outputNamesFilepath)
+        {
+            PdfDocument inputDocument = PdfReader.Open(inputFilePath, PdfDocumentOpenMode.Import);
+            string[] outputFileNames = splitNames(outputNamesFilepath);
+            string name = Path.GetFileNameWithoutExtension(inputFilePath);
+            for (int i = 0; i < inputDocument.PageCount; i++)
+            {
+                PdfDocument outputDocument = new PdfDocument();
+                outputDocument.Info.Title = outputFileNames[i];
+                outputDocument.AddPage(inputDocument.Pages[i]);
+                string output = Path.Combine(outputFilePath, outputFileNames[i] + ".pdf");
+                outputDocument.Save(output);
+                outputDocument.Close();
+            }
+            inputDocument.Close();
+        }
+        private static string[] splitNames(string outputNamesFilepath)
+        {
+            TextFieldParser parser = new TextFieldParser(outputNamesFilepath);
+
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            string[] names = null;
+            while (!parser.EndOfData)
+            {
+                names = parser.ReadFields();
+                  
+            }
+            return names;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PdfSplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilepath = @"C:\Users\Eduardo\source\repos\PdfSplitter\test.pdf";
+            var outputFilepath = @"C:\Users\Eduardo\source\repos\PdfSplitter";
+            var outputNamesFilepath = @"C:\Users\Eduardo\source\repos\PdfSplitter\test.csv";
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            PdfSplitter.PfSplitter.Split(inputFilepath, outputFilepath, outputNamesFilepath);
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
